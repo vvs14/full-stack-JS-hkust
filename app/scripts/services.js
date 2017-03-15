06f39656df386de5f6e6d2b8ae4d8049feff9fec
Vivek <vivekvsingh14@gmail.com>
2017-03-15 21:33:58 +0530

    Factory & services: fixed a typo

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('conryfusionApp')
+angular.module('confusionApp')
     .factory('menuFactory', function() {
         var dishes = [{
                 name: 'Uthapizza',
